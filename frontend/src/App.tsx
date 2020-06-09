726cee9b57d5d1b8c0b6fe38f1d079e95d4d2b08
Dennis Peters <denjpeters@gmail.com>
2020-06-09 17:02:28 -0400

    Can pass in a parameter via post

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.scss';
+import axios from "axios";
 
 const App = () => {
+    useEffect(() => {
+        // axios.defaults.withCredentials = true;
+        axios.post('/api', {single: 3})
+            .then((response: any) => {
+                console.log(response.data);
+            });
+    });
     return (
         <>
-            App is here
-        </>
+            App is here </>
     );
 };
 
--- server/Server.ts
+++ server/Server.ts
@@ -1,23 +1,25 @@
 import {Express, Request, Response} from "express";
 import express from "express";
 import * as path from "path";
+import bodyParser from "body-parser";
 
 export class Server {
-
     private app: Express;
 
     constructor(app: Express) {
         this.app = app;
 
         this.app.use(express.static(path.resolve("./") + "/build/frontend"));
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({extended: true}));
 
-        this.app.get("/api", (req: Request, res: Response): void => {
+        this.app.post("/api", (req: Request, res: Response): void => {
             // res.sendStatus(202);
             // res.send("You have reached the API!");
+            // console.log(req.body);
             res.setHeader('Content-Type', 'application/json');
-            res.json({One: 1, Two: 2, Three: "Tres"});
+            res.json({One: req.body.single, Two: 2, Three: "Tres"});
         });
-
 
         this.app.get("*", (req: Request, res: Response): void => {
             res.sendFile(path.resolve("./") + "/build/frontend/index.html");
@@ -27,6 +29,5 @@ export class Server {
     public start(port: number): void {
         this.app.listen(port, () => console.log(`Server listening on port ${port}!`));
     }
-
 }
 
