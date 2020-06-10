5ea128f2cdbb68fbb7d215c8329daf352588884a
Dennis Peters <denjpeters@gmail.com>
2020-06-10 10:16:05 -0400

    Builds server & react app

--- src-server/src/Server.ts
+++ src-server/src/Server.ts
@@ -2,7 +2,7 @@ import {Express, Request, Response} from "express";
 import express from "express";
 import * as path from "path";
 import bodyParser from "body-parser";
-import {testFunction} from "src-common/src/functionality";
+// import {testFunction} from "src-common/src/functionality";
 
 export class Server {
     private app: Express;
@@ -10,7 +10,7 @@ export class Server {
     constructor(app: Express) {
         this.app = app;
 
-        this.app.use(express.static(path.resolve("./") + "/build/frontend"));
+        this.app.use(express.static(path.resolve("./") + "/build/app"));
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({extended: true}));
 
@@ -18,14 +18,14 @@ export class Server {
             // res.sendStatus(202);
             // res.send("You have reached the API!");
             // console.log(req.body);
-            testFunction();
+            // testFunction();
 
             res.setHeader('Content-Type', 'application/json');
             res.json({One: req.body.single, Two: 2, Three: "Tres"});
         });
 
         this.app.get("*", (req: Request, res: Response): void => {
-            res.sendFile(path.resolve("./") + "/build/frontend/index.html");
+            res.sendFile(path.resolve("./") + "/build/app/index.html");
         });
     }
 
