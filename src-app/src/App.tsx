47b7b48e56b20d109d721fefc2492e3c9ffe959a
Dennis Peters <denjpeters@gmail.com>
2020-06-11 11:00:37 -0400

    Common compiles into server just fine

--- src-app/src/App.tsx
+++ src-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import './App.scss';
 import axios from "axios";
+// import {testFunction} from "src-common/src/functionality";
 
 const App = () => {
     useEffect(() => {
