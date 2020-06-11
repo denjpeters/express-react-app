856adec63e2016cd8eaf5255d3c3ce38eba6013b
Dennis Peters <denjpeters@gmail.com>
2020-06-10 23:52:15 -0400

    Fixed build issue

--- src-common/src/functionality.ts
+++ src-common/src/functionality.ts
@@ -1,7 +1,12 @@
+
 export const testFunction = () => {
     console.log('Test function, again');
-};
+}
 
-export const namedItemResolution = (sql: string, values: any) => {
-    console.log(sql, values);
-};
+export const testFunction2 = () => {
+    console.log('Test function, again');
+}
+
+// export const namedItemResolution = (sql: string, values: any) => {
+//     console.log(sql, values);
+// }
